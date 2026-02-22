b0aa0e4e8d95054c046e7fbf4d2f185c6eefd72b
Diego Fernando Marin <dfmarin@gmail.com>
2026-02-22 14:46:05 -0500

    feat: programa principal

---
+++ main.py
@@ -0,0 +1,18 @@
+def suma(a: float, b: float) -> float:
+    # TODO: Agrega el DocString
+    # TODO: Implementa la función
+    return 0.0
+
+
+def es_par(n: int) -> bool:
+    # TODO: Agrega el DocString
+    # TODO: Implementa la función
+    return False
+
+
+if __name__ == "__main__":
+    # Ejemplos de uso
+    print(f"Suma de 5 + 3 = {suma(5, 3)}")
+    print(f"¿El número 4 es par? {es_par(4)}")
+    print(f"¿El número 7 es par? {es_par(7)}")
+
